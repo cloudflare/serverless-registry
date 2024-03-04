19eb68f7a1a758f40348b005a57cfea033ab56c8
Chongyi Zheng <git@zcy.dev>
2024-03-03 20:01:23 -0500

    Remove `threads: false` in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,8 +10,5 @@ export default defineConfig({
       r2Buckets: ["REGISTRY"],
       kvNamespaces: ["UPLOADS"],
     },
-    // ? https://github.com/vitest-dev/vitest/issues/2008
-    // @ts-ignore
-    threads: false,
   },
 });
