2e69f47383acc84ab18f615e6396aad5aaa7c7be
Gabi Villalonga Simon <gvillalongasimon@cloudflare.com>
2024-05-31 18:09:23 -0500

    fix: return a response that docker understands better when it doesn't find a manifest

--- src/v2-errors.ts
+++ src/v2-errors.ts
@@ -1,19 +1,24 @@
-export const ManifestUnknownError = {
-  errors: [
-    {
-      code: "MANIFEST_UNKNOWN",
-      message: "manifest unknown",
-      detail: "This error is returned when the manifest, identified by name and tag is unknown to the repository.",
-    },
-  ],
-};
+export const ManifestUnknownError = (tag: string) =>
+  ({
+    errors: [
+      {
+        code: "MANIFEST_UNKNOWN",
+        message: "manifest unknown",
+        detail: {
+          Tag: tag,
+        },
+      },
+    ],
+  } as const);
 
 export const BlobUnknownError = {
   errors: [
     {
       code: "BLOB_UNKNOWN",
       message: "blob unknown to registry",
-      detail: "This error may be returned when a manifest blob is unknown to the registry.",
+      detail: {
+        message: "This error may be returned when a layer blob is unknown to the registry.",
+      },
     },
   ],
 };
