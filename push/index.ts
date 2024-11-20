72b17555bb17776392b06828aff5d36249fc5723
pierre818181 <pierre.bastola@runpod.io>
2024-11-20 12:43:24 -0800

    temp changes to use oci manifest

--- push/index.ts
+++ push/index.ts
@@ -39,33 +39,34 @@ if (installBun.exitCode !== 0) {
 
 
 console.log("Preparing image...");
-const imageMetadataRes = await $`/usr/bin/docker images --format "{{ .ID }}" ${image}`;
-if (imageMetadataRes.exitCode !== 0) {
-  console.error(
-    "Image",
-    image,
-    "doesn't exist. The docker daemon might not be running, or the image doesn't exist. Check your existing images with\n\n\tdocker images",
-  );
-  process.exit(1);
-}
+// const imageMetadataRes = await $`/usr/bin/docker images --format "{{ .ID }}" ${image}`;
+// if (imageMetadataRes.exitCode !== 0) {
+//   console.error(
+//     "Image",
+//     image,
+//     "doesn't exist. The docker daemon might not be running, or the image doesn't exist. Check your existing images with\n\n\tdocker images",
+//   );
+//   process.exit(1);
+// }
 
-const imageID = imageMetadataRes.text();
-if (imageID === "") {
-  console.error("Image", image, "doesn't exist. Check your existing images with\n\n\tdocker images");
-  process.exit(1);
-}
+// const imageID = imageMetadataRes.text();
+// if (imageID === "") {
+//   console.error("Image", image, "doesn't exist. Check your existing images with\n\n\tdocker images");
+//   process.exit(1);
+// }
 
 console.log(`Image ${image} found locally, saving to disk...`);
 
-const tarFile = imageID.trim() + ".tar";
+// const tarFile = imageID.trim() + ".tar";
+const tarFile = 'output.tar'
 const imagePath = ".output-image";
-if (!(await file(tarFile).exists())) {
-  const output = await $`/usr/bin/docker save ${image} --output ${tarFile}`;
+if ((await file(tarFile).exists())) {
+  // const output = await $`/usr/bin/docker save ${image} --output ${tarFile}`;
 
-  if (output.exitCode != 0) {
-    console.error("Error saving image", image, output.text());
-    process.exit(1);
-  }
+  // if (output.exitCode != 0) {
+  //   console.error("Error saving image", image, output.text());
+  //   process.exit(1);
+  // }
 
   console.log(`Image saved as ${tarFile}, extracting...`);
 
