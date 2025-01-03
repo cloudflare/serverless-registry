f8133bbb636bddfe4ceec3c194bf1550dbd26126
Dramelac <dramelac@users.noreply.github.com>
2025-01-03 16:17:40 +0100

    Remove sha256 manifest from the tag list + Handle invalid n values + Dynamically add the next link if needed

--- src/router.ts
+++ src/router.ts
@@ -524,20 +524,39 @@ v2Router.get("/:name+/tags/list", async (req, env: Env) => {
 
   const { n: nStr = 50, last } = req.query;
   const n = +nStr;
-  if (isNaN(n)) {
+  if (isNaN(n) || n <= 0) {
     throw new ServerError("invalid 'n' parameter", 400);
   }
 
-  const tags = await env.REGISTRY.list({
+  let tags = await env.REGISTRY.list({
     prefix: `${name}/manifests`,
     limit: n,
     startAfter: last ? `${name}/manifests/${last}` : undefined,
   });
+  // Filter out sha256 manifest
+  let manifest_tags = tags.objects.filter( tag => !tag.key.startsWith(`${name}/manifests/sha256:`))
+  // If results are truncated and the manifest filter removed some result, extend the search to reach the n number of results expected by the client
+  while (tags.objects.length > 0 && tags.truncated && manifest_tags.length !== n) {
+    tags = await env.REGISTRY.list({
+      prefix: `${name}/manifests`,
+      limit: n - manifest_tags.length,
+      cursor: tags.cursor,
+    });
+    // Filter out sha256 manifest
+    manifest_tags = manifest_tags.concat(tags.objects.filter( tag => !tag.key.startsWith(`${name}/manifests/sha256:`)))
+  }
 
-  const keys = tags.objects.map((object) => object.key.split("/").pop()!);
+  const keys = manifest_tags.map((object) => object.key.split("/").pop()!);
   const url = new URL(req.url);
   url.searchParams.set("n", `${n}`);
   url.searchParams.set("last", keys.length ? keys[keys.length - 1] : "");
+  const response_headers: {"Content-Type":string, "Link"?:string} = {
+    "Content-Type": "application/json"
+  }
+  // Only supply a next link if the previous result is truncated
+  if (tags.truncated) {
+    response_headers.Link = `${url.toString()}; rel=next`
+  }
   return new Response(
     JSON.stringify({
       name,
@@ -545,10 +564,7 @@ v2Router.get("/:name+/tags/list", async (req, env: Env) => {
     }),
     {
       status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Link": `${url.toString()}; rel=next`,
-      },
+      headers: response_headers,
     },
   );
 });
